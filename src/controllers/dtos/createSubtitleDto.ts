c67f8c11d3af57508b6ad9cb0ca53bbfd7279219
Daniel Riccio <ricciodaniel98@gmail.com>
2023-12-02 18:03:37 -0500

    add lang for better organization

--- src/controllers/dtos/createSubtitleDto.ts
+++ src/controllers/dtos/createSubtitleDto.ts
@@ -1,19 +1,19 @@
 import { t } from 'elysia';
 import { FeatureType } from '../../types';
+import { languageCodesDto } from './languageCodesDto';
 
-export const CreateSubtitleDto = t.Object({
+export const createSubtitleDto = t.Object({
   file: t.File({ maxSize: '1m', type: 'text/plain' }),
   releaseName: t.String(),
   comments: t.Optional(t.String()),
-  featureDetails: t.Object({
-    featureType: t.Enum(FeatureType),
-    year: t.Number(),
-    title: t.String(),
-    featureName: t.String(),
-    imdbId: t.String(),
-    seasonNumber: t.Optional(t.Number({ minimum: 1 })),
-    episodeNumber: t.Optional(t.Number({ minimum: 1 })),
-  }),
+  featureType: t.Enum(FeatureType),
+  language: t.Union(languageCodesDto),
+  year: t.String(),
+  title: t.String(),
+  featureName: t.String(),
+  imdbId: t.String(),
+  seasonNumber: t.Optional(t.String()),
+  episodeNumber: t.Optional(t.String()),
 });
 
-export type CreateSubtitleTp = (typeof CreateSubtitleDto)['static'];
+export type createSubtitleTp = (typeof createSubtitleDto)['static'];
--- src/controllers/dtos/searchOptionsDto.ts
+++ src/controllers/dtos/searchOptionsDto.ts
@@ -1,13 +1,14 @@
 import { t } from 'elysia';
 import { FeatureType } from '../../types';
+import { languageCodesDto } from './languageCodesDto';
 
-export const SearchOptionsDto = t.Object({
+export const searchOptionsDto = t.Object({
   imdbId: t.String(),
-  language: t.String(),
+  language: t.Union(languageCodesDto),
   query: t.Optional(t.String()),
   featureType: t.Enum(FeatureType),
   episodeNumber: t.Optional(t.Number({ minimum: 1 })),
   seasonNumber: t.Optional(t.Number({ minimum: 1 })),
 });
 
-export type SearchOptionsTp = (typeof SearchOptionsDto)['static'];
+export type searchOptionsTp = (typeof searchOptionsDto)['static'];
