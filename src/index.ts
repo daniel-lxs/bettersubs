9d9666a9b3ae5cbd3da95f3285821439109440cc
Daniel Riccio <ricciodaniel98@gmail.com>
2023-12-05 21:19:04 -0500

    use cors temporarily

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,40 @@
 import { Elysia } from 'elysia';
+import cors from '@elysiajs/cors';
 import createLogger from 'logging';
+
 import getEnvOrThrow from './helpers/getOrThrow';
 import { subtitlesController } from './controllers/subtitlesController';
 
+const logger = createLogger('Main');
+
 const port = parseInt(getEnvOrThrow('PORT'));
-const app = new Elysia().onError(({ code, error, set }) => {
-  logger.error(`${error.message} ${error.cause} ${error.stack}`);
-  switch (code) {
-    case 'NOT_FOUND':
-      set.status = 404;
-      return 'Not Found :(';
+let app = new Elysia()
+  .use(cors())
+  /*.use(
+    rateLimit({
+      duration: 2000,
+      max: 5,
+    })
+  )*/
+  .onError(({ code, error, set }) => {
+    logger.error(`${error.message} ${error.cause} ${error.stack}`);
+    switch (code) {
+      case 'NOT_FOUND':
+        set.status = 404;
+        return 'Not Found :(';
 
-    case 'INTERNAL_SERVER_ERROR':
-      set.status = 500;
-      return 'Internal Server Error';
+      case 'INTERNAL_SERVER_ERROR':
+        set.status = 500;
+        return 'Internal Server Error';
 
-    case 'PARSE':
-      set.status = 400;
-      return 'Parse Error';
-  }
-});
-const logger = createLogger('App');
+      case 'PARSE':
+        set.status = 400;
+        return 'Parse Error';
+    }
+  });
 
 //Use controller
-subtitlesController(app, logger);
+app = subtitlesController(app);
 
 app.listen(port);
 
