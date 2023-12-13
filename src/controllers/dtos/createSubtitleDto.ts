1dff64cddc7660599efb01f5045fb128f0decf39
Daniel Riccio <ricciodaniel98@gmail.com>
2023-12-12 21:01:43 -0500

    fix mime type

--- src/controllers/dtos/createSubtitleDto.ts
+++ src/controllers/dtos/createSubtitleDto.ts
@@ -2,7 +2,7 @@ import { t } from 'elysia';
 import { languageCodesDto } from './languageCodesDto';
 
 export const createSubtitleDto = t.Object({
-  file: t.File({ maxSize: '1m', type: 'text/plain' }),
+  file: t.File({ maxSize: '1m', type: 'application/x-subrip' }),
   imdbId: t.String(),
   releaseName: t.String(),
   comments: t.Optional(t.String()),
