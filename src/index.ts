94518a715285d4c236d646bd65fa5aefb28dc955
Daniel Riccio <ricciodaniel98@gmail.com>
2023-12-26 18:59:37 -0500

    remove conflicting error handler

--- src/index.ts
+++ src/index.ts
@@ -9,30 +9,13 @@ import { usersController } from './controllers/usersController';
 const logger = createLogger('Main');
 
 const port = parseInt(getEnvOrThrow('PORT'));
-let app = new Elysia()
-  .use(cors())
-  /*.use(
+let app = new Elysia().use(cors());
+/*.use(
     rateLimit({
       duration: 2000,
       max: 5,
     })
   )*/
-  .onError(({ code, error, set }) => {
-    logger.error(`${error.message} ${error.cause} ${error.stack}`);
-    switch (code) {
-      case 'NOT_FOUND':
-        set.status = 404;
-        return 'Not Found :(';
-
-      case 'INTERNAL_SERVER_ERROR':
-        set.status = 500;
-        return 'Internal Server Error';
-
-      case 'PARSE':
-        set.status = 400;
-        return 'Parse Error';
-    }
-  });
 
 //Use controller
 app = subtitlesController(app);
