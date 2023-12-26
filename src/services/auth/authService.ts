0044be6d9291648073e0f40a692f63e8c1bbbf94
Daniel Riccio <ricciodaniel98@gmail.com>
2023-12-26 18:58:09 -0500

    use async methods

--- src/services/auth/authService.ts
+++ src/services/auth/authService.ts
@@ -3,8 +3,8 @@ import { RegisterUserDto } from '../../controllers/dtos';
 import { createUser } from '../../data/repositories/userRepository';
 import { User } from '../../types/User';
 
-export function registerUser(user: RegisterUserDto): User {
-  const hash = hashPassword(user.password);
+export async function registerUser(user: RegisterUserDto): Promise<User> {
+  const hash = await hashPassword(user.password);
 
   const createdUser = createUser({
     username: user.username,
@@ -14,11 +14,14 @@ export function registerUser(user: RegisterUserDto): User {
   });
   return createdUser;
 }
-function hashPassword(password: string): string {
+async function hashPassword(password: string): Promise<string> {
   const saltRounds = 10;
-  const salt = bcrypt.genSaltSync(saltRounds);
-  return bcrypt.hashSync(password, salt);
+  const salt = await bcrypt.genSalt(saltRounds);
+  return await bcrypt.hash(password, salt);
 }
-export function validatePassword(password: string, hash: string): boolean {
-  return bcrypt.compareSync(password, hash);
+export async function validatePassword(
+  password: string,
+  hash: string
+): Promise<boolean> {
+  return await bcrypt.compare(password, hash);
 }
